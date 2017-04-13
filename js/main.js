ff859db26a9e988e2fdd0aabf86866585b3ccf63
avk <akavasin@gmail.com>
2017-04-13 18:00:52 +0400

    OOP iteration - various fixes :)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-var workingSpace = document.querySelector('#main');
-var limitInputField = document.querySelector('#limit-field'); // Поле с суммой
-var adviserContainer = document.querySelector('#adviser-content-wrapper'); // Поле для вывода сообщений
-var countdownContainer = document.getElementById('countdown'); // Контейнер для таймера
-// Поп-ап со статистикой (финальный)
-var endPopUp = document.querySelector('#end-pop-up');
-// Поп-ап установка лимита
-var deadlineRange = document.querySelector('#deadline-range-field');
-var setlimitField = document.querySelector('#setlimit-field');
-// Поп-ап списание средств
-var setExpenseNameField = document.querySelector('#setexpense-name-field');
-var setExpenseValueField = document.querySelector('#setexpense-value-field');
-
 var simpl = {
   SEC_IN_DAY: 86400,
   secInPassedDays: 86400,
@@ -24,33 +11,51 @@ var simpl = {
   deadLinePeriod: localStorage.deadLinePeriod || this.deadLinePeriod || '',
   endDeadLine: localStorage.endDeadLine || this.endDeadLine || '',
   expenseItems: localStorage.expenseItems || this.expenseItems || [],
-  // setTimeout, setInterval
+  // DOM элементы
+  workingSpace: document.querySelector('#main'),
+  adviserContainer: document.querySelector('#adviser-content-wrapper'),
+  countdownContainer: document.querySelector('#countdown'),
+  limitInputField: document.querySelector('#limit-field'),
+  setlimitField: document.querySelector('#setlimit-field'),
+  deadlineRange: document.querySelector('#deadline-range-field'),
+  endPopUp: document.querySelector('#end-pop-up'),
+  // для setTimeout и setInterval
   messageHide: this.messageHide || '',
   animateAdvise: this.animateAdvise || '',
   // значения для установки лимита
   setLimitPopUpFill: function() {
-    this.initialLimit = parseInt(setlimitField.value, 10);
-    this.deadLinePeriod = parseInt(deadlineRange.value, 10);
     var setLimitPopUp = document.querySelector('#setlimit-pop-up');
+    this.initialLimit = parseInt(this.setlimitField.value, 10);
+    this.deadLinePeriod = parseInt(this.deadlineRange.value, 10);
     this.togglePopUpView(setLimitPopUp);
     this.setLimit();
+    this.clearPopUpFields();
+  },
+  addListeners: function() {
+    var self = simpl;
+
+    self.deadlineRange.addEventListener('input', function() {
+      var rangeOutput = document.querySelector('#deadline-range-output');
+      rangeOutput.innerHTML = self.deadlineRange.value;
+    });
+
+    self.workingSpace.addEventListener('click', self.btnAction);
   },
   // установка лимита
   setLimit: function() {
-    var self = this;
     this.cleanAdviser();
     if(this.deadLinePeriod < 1 || this.deadLinePeriod > 9 || this.initialLimit < 0 || isNaN(this.initialLimit) || this.initialLimit > 30000) {
       this.showSystemMessage(this.systemMessages.limitErrorTxt, this.systemMessages.messageType['error'], 10000);
       return 'Значение некорректно';
     }
-    limitInputField.value = this.initialLimit;
+    this.limitInputField.value = this.initialLimit;
     localStorage.setItem('initialLimit', this.initialLimit);
     localStorage.setItem('secInPassedDays', this.secInPassedDays);
     this.setInitialTimer();
     setTimeout(function() {
-      self.countdownWork();
-      countdownContainer.style.display = 'block';
-    }, 1000);
+      this.countdownWork();
+      this.countdownContainer.style.display = 'block';
+    }.bind(this), 1000);
     this.controlsState();
     this.currentLimit = this.initialLimit;
     localStorage.setItem('currentLimit', this.currentLimit);
@@ -69,9 +74,8 @@ var simpl = {
   },
   // запуск и работа
   countdownWork: function() {
-    var self = this;
     if(!this.countdownIsOn) {
-      countdownContainer.innerHTML = '';
+      this.countdownContainer.innerHTML = '';
     } else {
       var now = new Date();
       now = Math.floor((this.endDeadLine - now) / 1000);
@@ -86,7 +90,7 @@ var simpl = {
           localStorage.setItem('secInPassedDays', this.secInPassedDays);
         }
         this.countdownIsOn = false;
-        this.togglePopUpView(endPopUp);
+        this.togglePopUpView(this.endPopUp);
         this.endPopUpFill();
         return false;
       }
@@ -114,34 +118,37 @@ var simpl = {
         thour = '0' + thour;
       }
 
-      countdownContainer.innerHTML = thour + ':' + tmin + ':' + tsec;
+      this.countdownContainer.innerHTML = thour + ':' + tmin + ':' + tsec;
 
       setTimeout(function() {
-        self.countdownWork();
-      }, 1000);
+        this.countdownWork();
+      }.bind(this), 1000);
     }
     return true;
   },
    // значения для списания
   setExpensePopUpFill: function() {
     var setExpensePopUp = document.querySelector('#setexpense-pop-up');
+    var setExpenseValueField = document.querySelector('#setexpense-value-field');
     this.expense = parseInt(setExpenseValueField.value, 10);
-    this.togglePopUpView(setExpensePopUp);
     this.limitSubtract();
+    this.togglePopUpView(setExpensePopUp);
+    this.clearPopUpFields();
   },
   // cписание средств
   limitSubtract: function() {
+    var setExpenseNameField = document.querySelector('#setexpense-name-field');
     if(Number.isInteger(this.expense) && this.expense > 0 && this.expense < 30000 && setExpenseNameField.value) {
       this.currentLimit = localStorage.currentLimit || this.initialLimit;
       this.currentLimit -= this.expense;
       localStorage.setItem('currentLimit', this.currentLimit);
-      limitInputField.value = this.currentLimit;
+      this.limitInputField.value = this.currentLimit;
       this.addExpenseItem(setExpenseNameField.value);
       this.checkColorIndicator();
 
       if(this.currentLimit < -30000) {
         this.countdownIsOn = false;
-        this.togglePopUpView(endPopUp);
+        this.togglePopUpView(this.endPopUp);
         this.endPopUpFill();
         return 'Вы превысили лимит';
       }
@@ -164,6 +171,7 @@ var simpl = {
     localStorage.setItem('expenseItems', this.expenseItems);
     this.showExpenseList();
   },
+  // отображение списка трат
   showExpenseList: function() {
     this.cleanAdviser();
     if(localStorage.expenseItems) {
@@ -171,32 +179,39 @@ var simpl = {
     }
     var expenseList = document.createElement('ul');
     expenseList.classList.add('expense-list');
-    adviserContainer.appendChild(expenseList);
+    this.adviserContainer.appendChild(expenseList);
     expenseList.innerHTML = this.expenseItems.map(function(i) {
       return '<li>' + i + '</li>';
     }).join('');
   },
   // запуск приложения при повторной загрузке
   init: function() {
-    this.startDeadLine = new Date(Date.parse(localStorage.startDeadLine));
-    this.endDeadLine = new Date(Date.parse(localStorage.endDeadLine));
-    this.deadLinePeriod = localStorage.deadLinePeriod;
-    this.secInPassedDays = localStorage.secInPassedDays;
-    this.countdownIsOn = true;
-    this.controlsState();
-    this.checkColorIndicator();
-    this.countdownWork();
-    this.showExpenseList();
-    clearTimeout(showTimer);
-    var showTimer = setTimeout(function() {
-      countdownContainer.style.display = 'block';
-    }, 1000);
+    if(localStorage.currentLimit && localStorage.currentLimit < -30000) {
+      this.countdownIsOn = false;
+      this.togglePopUpView(this.endPopUp);
+      this.endPopUpFill();
+    } else {
+      this.startDeadLine = new Date(Date.parse(localStorage.startDeadLine));
+      this.endDeadLine = new Date(Date.parse(localStorage.endDeadLine));
+      this.deadLinePeriod = localStorage.deadLinePeriod;
+      this.secInPassedDays = localStorage.secInPassedDays;
+      this.countdownIsOn = true;
+      this.controlsState();
+      this.checkColorIndicator();
+      this.countdownWork();
+      this.showExpenseList();
+      this.limitInputField.value = this.currentLimit;
+      clearTimeout(showTimer);
+      var showTimer = setTimeout(function() {
+        this.countdownContainer.style.display = 'block';
+      }.bind(this), 1000);
+    }
   },
   // обновление текущего лимита раз в сутки
   renewLimit: function(sum) {
     this.currentLimit = parseInt(localStorage.currentLimit, 10) || parseInt(localStorage.initialLimit, 10);
     this.currentLimit += sum;
-    limitInputField.value = this.currentLimit;
+    this.limitInputField.value = this.currentLimit;
     localStorage.setItem('currentLimit', this.currentLimit);
     this.checkColorIndicator();
   },
@@ -205,15 +220,15 @@ var simpl = {
     this.countdownIsOn = false;
     this.controlsState();
     this.cleanAdviser();
-    localStorage.clear();
     this.initialLimit = '';
     this.currentLimit = '';
     this.startDeadLine = '';
     this.endDeadLine = '';
     this.deadLinePeriod = '';
     this.expenseItems = [];
-    limitInputField.value = '';
-    setlimitField.value = '';
+    this.limitInputField.value = '';
+    this.setlimitField.value = '';
+    localStorage.clear();
     document.querySelectorAll('.pop-up').forEach(function(p) {
       p.style.display = '';
     });
@@ -227,7 +242,7 @@ var simpl = {
     this.initialLimit = parseInt(localStorage.initialLimit, 10);
     this.currentLimit = parseInt(localStorage.currentLimit, 10);
     this.deadLinePeriod = parseInt(localStorage.deadLinePeriod, 10);
-    limitInputField.value = this.currentLimit;
+    this.limitInputField.value = this.currentLimit;
 
     if(!this.currentLimit || this.currentLimit > -30000) {
       endVerdict.innerHTML = '<p>Время истекло</p><br>';
@@ -261,9 +276,9 @@ var simpl = {
   // индикатор отрицательного баланса
   checkColorIndicator: function() {
     if(parseInt(localStorage.currentLimit, 10) < 0) {
-      limitInputField.classList.add('negative-balance');
+      this.limitInputField.classList.add('negative-balance');
     } else {
-      limitInputField.classList.remove('negative-balance');
+      this.limitInputField.classList.remove('negative-balance');
     }
   },
   // состояние кнопок
@@ -279,7 +294,7 @@ var simpl = {
         btn.disabled = true;
         btn.classList.add('inactive');
       });
-      limitInputField.style.fontSize = '0.9em';
+      this.limitInputField.style.fontSize = '0.9em';
     } else {
       inProcessBtns.forEach(function(btn) {
         btn.disabled = false;
@@ -289,7 +304,7 @@ var simpl = {
         btn.disabled = true;
         btn.classList.add('inactive');
       });
-      limitInputField.style.fontSize = '1.25em';
+      this.limitInputField.style.fontSize = '1.25em';
     }
   },
   // отображение поп-апов
@@ -300,32 +315,53 @@ var simpl = {
       pop.style.display = 'flex';
     }
   },
+  clearPopUpFields: function() {
+    var popUpFields = document.querySelectorAll('.pop-up-field');
+    popUpFields.forEach(function(field) {
+      field.value = '';
+    });
+  },
   // очистка поля с сообщением
   cleanAdviser: function() {
     clearInterval(this.animateAdvise);
     clearTimeout(this.messageHide);
-    adviserContainer.innerHTML = '';
-    adviserContainer.className = '';
+    this.adviserContainer.innerHTML = '';
+    this.adviserContainer.className = '';
+  },
+  btnAction: function(evt) {
+    var self = simpl;
+    var id = evt.target.id;
+    var popId = evt.target.dataset.popId;
+    var popup = document.getElementById(popId);
+    if(id === 'set-limit-btn' || id === 'reset-btn' || id === 'limit-subtract-btn' || id === 'restart-cancel' || id === 'setlimit-cancel' || id === 'setexpense-cancel') {
+      self.togglePopUpView(popup);
+    } else if(id === 'end-restart' || id === 'restart-confirm') {
+      self.togglePopUpView(popup);
+      self.restart();
+    } else if(id === 'setlimit-submit') {
+      self.setLimitPopUpFill();
+    } else if(id === 'setexpense-submit') {
+      self.setExpensePopUpFill();
+    }
   },
   // вывод системных сообщений (с анимацией)
   showSystemMessage: function(systemMessageText, messageType, expandTime) {
     var i = 0;
-    var self = this;
     var blinkingCursor = '<span class="blinking-cursor">&nbsp;</span>';
     this.cleanAdviser();
     this.animateAdvise = setInterval(function() {
-      adviserContainer.innerHTML += systemMessageText[i];
+      this.adviserContainer.innerHTML += systemMessageText[i];
       i++;
       if(i === systemMessageText.length) {
-        clearInterval(self.animateAdvise);
-        adviserContainer.innerHTML += blinkingCursor;
-        adviserContainer.classList.add(messageType);
+        clearInterval(this.animateAdvise);
+        this.adviserContainer.innerHTML += blinkingCursor;
+        this.adviserContainer.classList.add(messageType);
       }
-    }, 30); // магическое число
+    }.bind(this), 30); // магическое число
     this.messageHide = setTimeout(function() {
-      self.cleanAdviser();
-      self.showExpenseList();
-    }, expandTime = expandTime || 5000);
+      this.cleanAdviser();
+      this.showExpenseList();
+    }.bind(this), expandTime = expandTime || 5000);
   },
   // объект с сообщениями
   systemMessages: {
@@ -340,62 +376,22 @@ var simpl = {
 
 // Условия при загрузке страницы
 window.onload = function() {
-  if(localStorage.currentLimit && localStorage.currentLimit > -30000) {
+  simpl.addListeners();
+  if(localStorage.length) { // повторная загрузка
     simpl.init();
-    simpl.currentLimit = localStorage.currentLimit;
-    limitInputField.value = simpl.currentLimit;
-  } else if(localStorage.currentLimit && localStorage.currentLimit < -30000) {
-    simpl.countdownIsOn = false;
-    simpl.togglePopUpView(endPopUp);
-    simpl.endPopUpFill();
-  } else if(localStorage.initialLimit) {
-    simpl.init();
-    simpl.initialLimit = localStorage.initialLimit;
-    limitInputField.value = simpl.initialLimit;
   } else {
     simpl.controlsState();
   }
 };
 
-// Обработчики по кликам на кнопки
-workingSpace.addEventListener('click', function(evt) {
-  var id = evt.target.id;
-  var popId = evt.target.dataset.popId;
-  var popup = document.getElementById(popId);
-  if(id === 'set-limit-btn' || id === 'reset-btn' || id === 'limit-subtract-btn' || id === 'restart-cancel') {
-    simpl.togglePopUpView(popup);
-  } else if(id === 'end-restart' || id === 'restart-confirm') {
-    simpl.togglePopUpView(popup);
-    simpl.restart();
-  } else if(id === 'setlimit-cancel') {
-    simpl.togglePopUpView(popup);
-    setlimitField.value = '';
-  } else if(id === 'setexpense-cancel') {
-    simpl.togglePopUpView(popup);
-    setExpenseValueField.value = '';
-    setExpenseNameField.value = '';
-  } else if(id === 'setlimit-submit') {
-    simpl.setLimitPopUpFill();
-    setlimitField.value = '';
-  } else if(id === 'setexpense-submit') {
-    simpl.setExpensePopUpFill();
-    setExpenseValueField.value = '';
-    setExpenseNameField.value = '';
-  }
-});
-
-deadlineRange.addEventListener('input', function() {
-  var rangeOutput = document.querySelector('#deadline-range-output');
-  rangeOutput.innerHTML = deadlineRange.value;
-});
-
 // Цели
 
-// 1. Имплементировать список покупок
+// 1. Имплементировать список покупок (готово)
+// 2. В списке покупок, напротив наименования мелким шрифтом писать сумму
+// 3. Оптимизировать simpl.init - чет каша какая-то по ощущениям :)
 
 // Баги
 
-// 1. Разобраться с потерей контекста в методах countdownWork и showSystemMessage объекта simpl
-// (в обоих случаях используется setTimeout) - решил с var self = this
+// 1. Разобраться с потерей контекста в методах countdownWork и showSystemMessage объекта simpl (готово - забиндил ыыы )
 //
 //

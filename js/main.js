30f1eac2eb8854daecfd4239669db3cb6e953d42
Alexey Kavasin (work) <akavasin@gmail.com>
2017-08-21 18:14:08 +0400

    minor fixes

--- js/main.js
+++ js/main.js
@@ -480,10 +480,11 @@ const simpl = {
   // register sw script in supporting browsers
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js', { scope: '.' }).then(() => {
-      console.log('Service Worker registered successfully.');
-    }).catch(error => {
-      console.log('Service Worker registration failed:', error);
-    });
+        console.log('Service Worker registered successfully.');
+      }).catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+    }
   },
 
   // messages object

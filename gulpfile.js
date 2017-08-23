fe7bcfda3fa8ad7d98f3635f5804269ed4ddf183
Alexey Kavasin (work) <akavasin@gmail.com>
2017-08-23 16:15:40 +0400

    imgs moved to folder, src-s, manifest json start url

--- gulpfile.js
+++ gulpfile.js
@@ -87,6 +87,7 @@ gulp.task('serve', function() {
 
   gulp.watch('sass/**/*.{scss,sass}', ['style']);
   gulp.watch('*.html').on('change', server.reload);
+  gulp.watch('js/main.js', ['babelify', 'compress']);
   gulp.watch('js/main.js').on('change', server.reload);
 });
 

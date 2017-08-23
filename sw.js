ed80e7093950ca584ec685eacd64d59aeb6e468c
Alexey Kavasin (work) <akavasin@gmail.com>
2017-08-23 17:23:19 +0400

    more added to cache, self to this

--- sw.js
+++ sw.js
@@ -1,20 +1,28 @@
-self.addEventListener('install', e => {
+this.addEventListener('install', function(e) {
   e.waitUntil(
   // after the service worker is installed,
   // open a new cache
-  caches.open('simpl').then(cache => {
-  // add all URLs of resources we want to cache
-    return cache.addAll([
-      'index.html',
-      'css/main.min.css',
-      'js/main.min.js'
-    ]);
-   })
- );
+    caches.open('simpl').then(function(cache) {
+    // add all URLs of resources we want to cache
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'css/main.css',
+        'css/main.min.css',
+        'js/main.js',
+        'js/main.transp.js',
+        'js/main.min.js',
+        'fonts/Inconsolata-Regular.woff',
+        'fonts/Inconsolata-Regular.woff2',
+        'fonts/Inconsolata-Bold.woff',
+        'fonts/Inconsolata-Bold.woff2',
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
+this.addEventListener('fetch', function(event) {
+  //console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
